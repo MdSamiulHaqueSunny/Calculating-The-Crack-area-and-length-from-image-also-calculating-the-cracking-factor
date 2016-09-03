167a7e2854ec93b604acfffdae4b78ccf71781f7
MdSamiulHaqueSunny <mdshs31@gmail.com>
2016-09-03 15:54:19 +0600

    edited

--- Matlab Codes/previous_code.m
+++ Matlab Codes/previous_code.m
@@ -47,4 +47,34 @@ rightColumn =y2;
 width = bottomLine - topLine + 1;
 height = rightColumn - leftColumn + 1;
 PP = imcrop(I1,[topLine, leftColumn, width,height]);
-figure,imshow(PP);
+figure, imshow(PP);
+
+%Step 4: Convert the cropped RGB image of liner surface to grayscale image and then convert 
+%the grayscale image to bi-nary image
+K = rgb2gray(PP);
+figure, imshow(K);
+level = 0.30;
+bw = im2bw(K,level);
+bw = bwareaopen(bw, 250);
+figure, imshow(bw);
+
+%Step 5: Calculation of crack area and CIF
+
+a1=0; % number of black
+a0=0; % number of white
+for i=1:1:height
+for j=1:1:width
+vvvv(i,j)=bw(i,j);
+if bw(i,j)==0
+a1=a1+1;
+else
+a0=a0+1;
+end
+end
+end
+black_pixel=a1 %no of black
+white_pixel=a0 %no of white
+totalarea=240;
+crackarea=(totalarea/(a0+a1))*a1
+CIF=(crackarea/240)*100
+
